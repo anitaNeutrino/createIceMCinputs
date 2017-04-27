9d49eee9a5471a919b6d96ea62831bf928798211
Linda Cremonesi <l.cremonesi@ucl.ac.uk>
2017-04-27 11:14:30 +0100

    Add programs

---
+++ createIceMCSurfFromGps.cxx
@@ -0,0 +1,131 @@
+#include "AnitaConventions.h"
+#include "UsefulAnitaEvent.h"
+#include "RawAnitaEvent.h"
+#include "SurfHk.h"
+#include "TimedAnitaHeader.h"
+#include "ProgressBar.h"
+#include "PrettyAnitaHk.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TChain.h"
+#include "TCanvas.h"
+#include "TGraph.h"
+#include "TH2.h"
+#include "TColor.h"
+#include "TMath.h"
+#include "TStyle.h"
+#include "TSystem.h"
+#include <iostream>
+#include <fstream>
+
+using namespace Acclaim;
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+  if(!(argc==3 || argc==2)){
+    std::cerr << "Usage 1: " << argv[0] << " [run]" << std::endl;
+    std::cerr << "Usage 2: " << argv[0] << " [firstRun] [lastRun]" << std::endl;
+    return 1;
+  }
+
+  std::cout << argv[0] << "\t" << argv[1];
+  if(argc==3){std::cout << "\t" << argv[2];}
+  std::cout << std::endl;
+  const Int_t firstRun = atoi(argv[1]);
+  const Int_t lastRun = argc==3 ? atoi(argv[2]) : firstRun;
+
+
+  FileStat_t staty;
+  char surfName[1000];
+  // char gpsName[FILENAME_MAX];
+
+  AnitaVersion::set(3);
+
+  TFile *finput = new TFile("anita3gps_pitchroll_3.root", "read");
+  TTree *icemcGpsTree = (TTree*)finput->Get("adu5PatTree");
+  icemcGpsTree->SetName("icemcGpsTree");
+  
+  Int_t icemcRealTime;
+  icemcGpsTree->SetBranchAddress("realTime", &icemcRealTime);
+
+  Int_t numGpsEntries = icemcGpsTree->GetEntries();
+
+  SurfHk *surf=NULL;
+  // Adu5Pat *pat = 0;
+  TChain *surfChain = new TChain("surfHkTree");
+  // TChain *gpsChain = new TChain("adu5PatTree");
+
+  for (int run=firstRun;run<lastRun+1; run++){
+    sprintf(surfName,"/unix/anita3/flight1415/root/run%d/surfHkFile%d.root",run,run);
+    // sprintf(gpsName,"/unix/anita3/flight1415/root/run%d/gpsFile%d.root",run,run);
+    if(gSystem->GetPathInfo(surfName,staty)) {
+      continue;
+    }
+    surfChain->Add(surfName);
+    // gpsChain->Add(gpsName);
+  }
+
+  surfChain->SetBranchAddress("surf",&surf);
+  // gpsChain->SetBranchAddress("pat", &pat);
+
+  surfChain->BuildIndex("surf->realTime");
+
+  UShort_t thresholds[2][48];
+  UShort_t scalers[2][48];
+  UInt_t realTime;
+
+  TTree* tree = new TTree();
+  tree->Branch("realTime",          &realTime,    "realTime/i"           );
+  tree->Branch("thresholds",        thresholds,   "thresholds[2][48]/s"  );
+  tree->Branch("scalers",           scalers,      "scalers[2][48]/s"     );
+
+
+  ProgressBar p(numGpsEntries);
+
+  cout << "Total entries " << numGpsEntries << endl;
+
+  for (UInt_t ientry=0; ientry<numGpsEntries; ientry++){
+    icemcGpsTree->GetEntry(ientry);
+    int index = surfChain->GetEntryNumberWithIndex(icemcRealTime);
+    
+    if (index<0){
+      //      cout << " I couldn't find " << icemcRealTime << " . Getting entry with best index." << endl;
+      index = surfChain->GetEntryNumberWithBestIndex(icemcRealTime);
+
+    } 
+    surfChain->GetEntry(index);
+    realTime = surf->realTime;
+    
+    if (TMath::Abs((int)icemcRealTime-(int)realTime)>60) cout << icemcRealTime << " " << realTime << endl;
+
+  
+    for (int ipol=AnitaTrigPol::kHorizontal;ipol!=AnitaTrigPol::kNotATrigPol;ipol++){
+      for(int iring=AnitaRing::kTopRing;iring!=AnitaRing::kNotARing;iring++){
+	for (int iphi=0;iphi<16;iphi++){
+	  thresholds[ipol-2][iring*16+iphi] = surf->getThreshold(iphi, (AnitaRing::AnitaRing_t)iring, (AnitaTrigPol::AnitaTrigPol_t)ipol);
+	  //	  cout << ipol << " " << iring*16+iphi <<  " " << thresholds[ipol][iring*16+iphi] << endl;
+	  scalers[ipol-2][iring*16+iphi] = surf->getScaler(iphi, (AnitaRing::AnitaRing_t)iring, (AnitaTrigPol::AnitaTrigPol_t)ipol);
+	}
+      }
+    }
+    
+    
+
+    tree->Fill();
+    
+    p++;
+    
+  }
+  
+  
+  cout << " Writing output " << endl;
+  TFile *output = new TFile (Form("partial/SampleSurf_run%d_anita3.root", firstRun), "recreate");
+  tree->Write("surf_icemc");
+  cout << "Done " << endl;
+  output->Close();
+  cout << "Closed " << endl;
+
+  return 0;
+}
---
+++ createIceMCTurfFromGps.cxx
@@ -0,0 +1,138 @@
+#include "AnitaConventions.h"
+#include "UsefulAnitaEvent.h"
+#include "RawAnitaEvent.h"
+#include "TurfRate.h"
+#include "SummedTurfRate.h"
+#include "TimedAnitaHeader.h"
+#include "PrettyAnitaHk.h"
+#include "ProgressBar.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TChain.h"
+#include "TCanvas.h"
+#include "TGraph.h"
+#include "TH2.h"
+#include "TColor.h"
+#include "TMath.h"
+#include "TStyle.h"
+#include "TSystem.h"
+#include <iostream>
+#include <fstream>
+
+using namespace Acclaim;
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+  if(!(argc==3 || argc==2)){
+    std::cerr << "Usage 1: " << argv[0] << " [run]" << std::endl;
+    std::cerr << "Usage 2: " << argv[0] << " [firstRun] [lastRun]" << std::endl;
+    return 1;
+  }
+
+  std::cout << argv[0] << "\t" << argv[1];
+  if(argc==3){std::cout << "\t" << argv[2];}
+  std::cout << std::endl;
+  const Int_t firstRun = atoi(argv[1]);
+  const Int_t lastRun = argc==3 ? atoi(argv[2]) : firstRun;
+
+
+  FileStat_t staty;
+  char turfName[FILENAME_MAX];
+  // char gpsName[FILENAME_MAX];
+
+  AnitaVersion::set(3);
+
+  TFile *finput = new TFile("anita3gps_pitchroll_3.root", "read");
+  TTree *icemcGpsTree = (TTree*)finput->Get("adu5PatTree");
+  icemcGpsTree->SetName("icemcGpsTree");
+  
+  Int_t icemcRealTime;
+  icemcGpsTree->SetBranchAddress("realTime", &icemcRealTime);
+
+  Int_t numGpsEntries = icemcGpsTree->GetEntries();
+
+
+  TurfRate *turf =0;
+  // Adu5Pat *pat = 0;
+  TChain *turfChain = new TChain("turfRateTree");
+  // TChain *gpsChain = new TChain("adu5PatTree");
+
+  for (int run=firstRun;run<lastRun+1; run++){
+    sprintf(turfName,"/unix/anita3/flight1415/root/run%d/turfRateFile%d.root",run,run);
+    // sprintf(gpsName,"/unix/anita3/flight1415/root/run%d/gpsFile%d.root",run,run);
+    if(gSystem->GetPathInfo(turfName,staty)) {
+      continue;
+    }
+    turfChain->Add(turfName);
+    // gpsChain->Add(gpsName);
+  }
+
+  turfChain->SetBranchAddress("turf",&turf);
+  // gpsChain->SetBranchAddress("pat", &pat);
+
+  turfChain->BuildIndex("turf->realTime");
+
+  UInt_t numEntries=turfChain->GetEntries();
+  cout << "There are " << numEntries << " in total "<< endl;
+
+
+  UInt_t phiTrigMask ;
+  UInt_t l1TrigMask ;
+  UInt_t phiTrigMaskH ;
+  UInt_t l1TrigMaskH ;
+  UInt_t realTime ;
+
+
+  TTree* tree = new TTree();
+  tree->Branch("realTime",     &realTime,     "realTime/i"    );
+  tree->Branch("phiTrigMask",  &phiTrigMask,  "phiTrigMask/I" );
+  tree->Branch("phiTrigMaskH", &phiTrigMaskH, "phiTrigMaskH/I");
+  tree->Branch("l1TrigMask",   &l1TrigMask,   "l1TrigMask/I"  );
+  tree->Branch("l1TrigMaskH",  &l1TrigMaskH,   "l1TrigMaskH/I" );
+
+
+  ProgressBar p(numGpsEntries);
+
+  for (unsigned int ientry=0; ientry<numGpsEntries; ientry++){
+    icemcGpsTree->GetEntry(ientry);
+    
+   
+    int index = turfChain->GetEntryNumberWithBestIndex(icemcRealTime);
+
+    if (index<0) {
+      realTime = icemcRealTime;
+      phiTrigMask=l1TrigMask=phiTrigMaskH=l1TrigMaskH=65536;
+
+      cout << "Can't find even best Index for this " << icemcRealTime << endl;
+
+    } else {
+
+      turfChain->GetEntry(index);
+      
+      realTime = turf->realTime;
+      int diffTime = (int)icemcRealTime - (int)realTime;
+
+      if (TMath::Abs(diffTime)>60) cout << icemcRealTime << " " << realTime << " " << diffTime << endl;
+      
+      phiTrigMask  = (turf->phiTrigMask );
+      l1TrigMask   = (turf->l2TrigMask  );
+      phiTrigMaskH = (turf->phiTrigMaskH);
+      l1TrigMaskH  = (turf->l2TrigMaskH );
+      
+    }
+    tree->Fill();
+      
+    p++;
+  }
+  
+  cout << "Creating output" << endl;
+  TFile *output = new TFile ("partial/SampleTurf_icemc_anita3.root", "recreate");
+  tree->Write("turfrate_icemc");
+  cout << "Done " << endl;
+  output->Close();
+  cout << "Closed " << endl;
+
+  return 0;
+
+}
